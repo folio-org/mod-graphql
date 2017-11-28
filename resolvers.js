bd3b500e1ced5aa007a3cf5f79a71afb7dd08c56
Jason Skomorowski <jason@indexdata.com>
2017-11-27 19:05:56 -0500

    Initial schema/resolvers for patron groups.

--- resolvers.js
+++ resolvers.js
@@ -14,7 +14,32 @@ export default {
 	});
       });
     },
+    groups: (root, args, context) => {
+      let okapi = context.okapi;
+      return fetch(`${okapi.url}/groups`, { headers: okapi.headers }).then((response) => {
+        return response.json().then(json => {
+          console.log('stuff', json.usergroups[0].metadata);
+          return json.usergroups;
+        });
+      });
+    },
   },
+
+  Metadata: {
+    createdByUser: (obj, args, { okapi }) => fetch(`${okapi.url}/users/${obj.createdByUserId}`,
+      { headers:okapi.headers })
+      .then(res => res.text().then(text => {
+        if (res.status < 400) return JSON.parse(text);
+        throw new Error(text);
+      })),
+    updatedByUser: (obj, args, { okapi }) => fetch(`${okapi.url}/users/${obj.updatedByUserId}`,
+      { headers:okapi.headers })
+      .then(res => res.text().then(text => {
+        if (res.status < 400) return JSON.parse(text);
+        throw new Error(text);
+      })),
+  },
+
 
   Mutation: {
     updateUser: (root, updated) => {
@@ -30,6 +55,38 @@ export default {
               throw new Error(text);
             }));
         });
+    },
+    createGroup: (root, { record }, { okapi }) => {
+      return fetch(`${okapi.url}/groups`,
+                    { headers: okapi.headers, method: 'POST', body: JSON.stringify(record) })
+        .then(res => res.text().then(text => {
+          if (res.status < 400) return JSON.parse(text);
+          throw new Error(text);
+        }));
+    },
+    updateGroup: (root, { id, record }, { okapi }) => {
+      let headers = Object.assign({}, okapi.headers, { 'Accept': 'text/plain' })
+      return fetch(`${okapi.url}/groups/${id}`, { headers: okapi.headers })
+        .then(res => res.json())
+        .then((orig) => {
+          const fullRecord = _.merge({}, orig, record);
+          delete(fullRecord.metadata);
+          return fetch(`${okapi.url}/groups/${id}`,
+                        { headers, method: 'PUT', body: JSON.stringify(fullRecord) },)
+            .then(res => res.text().then(text => {
+              if (res.status < 400) return fullRecord;
+              throw new Error(text);
+            }));
+        });
+    },
+    deleteGroup: (root, { record }, { okapi }) => {
+      let headers = Object.assign({}, okapi.headers, { 'Accept': 'text/plain' })
+      return fetch(`${okapi.url}/groups/${record.id}`,
+                    { headers, method: 'DELETE', })
+        .then(res => res.text().then(text => {
+          if (res.status < 400) return record;
+          throw new Error(text);
+        }));
     },
   },
 }
--- schema.js
+++ schema.js
@@ -2,6 +2,7 @@ export default `
   type Query {
     hello: String
     users(cql: String):[User]
+    groups:[Group]
   }
 
   type Mutation {
@@ -9,6 +10,16 @@ export default `
       id: ID!
       personal: PersonalInput
     ): User
+    createGroup (
+      record: GroupInput!
+    ): Group
+    updateGroup (
+      id: ID!
+      record: GroupInput!
+    ): Group
+    deleteGroup (
+      id: ID!
+    ): Group
   }
 
   type User {
@@ -26,10 +37,32 @@ export default `
     email: String
   }
 
+  type Group {
+    id: ID!
+    group: String!
+    desc: String
+    metadata: Metadata!
+    userCount: Int
+  }
+
+  type Metadata {
+    createdDate: String!
+    createdByUserId: ID!
+    createdByUser: User
+    updatedDate: String
+    updatedByUserId: ID
+    updatedByUser: User
+  }
+
   # This is a bit silly isn't it?
   input PersonalInput {
     lastName: String
     firstName: String
     email: String
+  }
+
+  input GroupInput {
+    group: String
+    desc: String
   }
 `;
