f31037dd2a5bda3b6f014987a72f9aeddbd9ff6a
Mike Taylor <mike@indexdata.com>
2018-04-10 16:35:52 +0100

    ESLint for the console-trace code

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ import app from './src/app';
 if (process.env.CONSOLE_TRACE) {
   // The code by Remy Sharp
   // https://remysharp.com/2014/05/23/where-is-that-console-log/
-  ['log', 'warn'].forEach(function(method) {
-    var old = console[method];
-    console[method] = function() {
-      var stack = (new Error()).stack.split(/\n/);
+  /* eslint-disable no-console */
+  ['log', 'warn'].forEach((method) => {
+    const old = console[method];
+    console[method] = (...callerArgs) => {
+      let stack = (new Error()).stack.split(/\n/);
       // Chrome includes a single "Error" line, FF doesn't.
       if (stack[0].indexOf('Error') === 0) {
         stack = stack.slice(1);
       }
-      var args = [].slice.apply(arguments).concat([stack[1].trim()]);
+      const args = callerArgs.concat([stack[1].trim()]);
       return old.apply(console, args);
     };
   });
