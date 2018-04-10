e305f8c7f037ed295f2677d5732b01780c8cb383
Mike Taylor <mike@indexdata.com>
2018-04-10 15:16:58 +0100

    Optionally log the original of all console-logging
    If the CONSOLE_TRACE environment is set.

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 import app from './src/app';
 
+if (process.env.CONSOLE_TRACE) {
+  // The code by Remy Sharp
+  // https://remysharp.com/2014/05/23/where-is-that-console-log/
+  ['log', 'warn'].forEach(function(method) {
+    var old = console[method];
+    console[method] = function() {
+      var stack = (new Error()).stack.split(/\n/);
+      // Chrome includes a single "Error" line, FF doesn't.
+      if (stack[0].indexOf('Error') === 0) {
+        stack = stack.slice(1);
+      }
+      var args = [].slice.apply(arguments).concat([stack[1].trim()]);
+      return old.apply(console, args);
+    };
+  });
+}
+
 app.listen(3000, '0.0.0.0');
