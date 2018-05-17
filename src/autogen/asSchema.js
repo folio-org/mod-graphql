142e0ba91e32b9c38b89cc3a7ce5d75ff4383bbe
Mike Taylor <mike@indexdata.com>
2018-05-17 12:37:44 +0100

    asSchema.render returns string instead of emitting it

--- src/autogen/asSchema.js
+++ src/autogen/asSchema.js
@@ -1,10 +1,12 @@
 function render(api, options) {
+  let output = '';
+
   ['title', 'version', 'protocols', 'baseUri'].forEach(tag => {
     const rendered = [];
     api.attributes(tag).forEach((attr) => {
       rendered.push(attr.plainValue());
     });
-    console.log(`# ${tag}: ${rendered.join(', ')}`);
+    output += `# ${tag}: ${rendered.join(', ')}`;
   });
 
   api.elementsOfKind('resources').forEach((resource, i) => {
@@ -15,12 +17,14 @@ function render(api, options) {
   function processResource(resource, level = 0, parentUri = '') {
     const uri = parentUri + resource.attr('relativeUri').plainValue();
     const dna = resource.attr('displayName');
-    console.log(`${'  '.repeat(level)}${uri}${dna ? ` # ${dna.plainValue()}` : ''}`);
+    output += `${'  '.repeat(level)}${uri}${dna ? ` # ${dna.plainValue()}` : ''}`;
 
     resource.elementsOfKind('resources').forEach((sub, i) => {
       processResource(sub, level+1, uri);
     });
   }
+
+  return output;
 }
 
 exports.render = render;
