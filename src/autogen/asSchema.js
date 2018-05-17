332df914b89af0ef0f7ccb041f7000af400527f5
Mike Taylor <mike@indexdata.com>
2018-05-17 18:40:16 +0100

    Pull processResource out of caller, rename to renderResource

--- src/autogen/asSchema.js
+++ src/autogen/asSchema.js
@@ -1,3 +1,46 @@
+function renderResource(resource, level = 0, parentUri = '') {
+  let output = '';
+  const rel = resource.attr('relativeUri').plainValue();
+  const uri = parentUri + rel;
+  // We will come back to pick up the types later
+
+  const methods = resource.elementsOfKind('methods');
+  methods.forEach((method) => {
+    // At this stage, we are only interested in GET, not mutations
+    if (method.name() === 'get') {
+      const args = [];
+
+      let basePath;
+      if (rel.startsWith('/{')) {
+        args.push(rel.replace(/\/{(.*)}/, '$1'));
+        basePath = parentUri;
+      } else {
+        basePath = uri;
+      }
+
+      const queryName = basePath.substr(1).replace('/', '-');
+      output += '  '.repeat(level) + queryName;
+      if (args.length > 0) {
+        output += `(${args.join(', ')})`;
+      }
+
+      const dna = resource.attr('displayName');
+      if (dna) {
+        output += ` # ${dna.plainValue()}`;
+      }
+
+      output += '\n';
+    }
+  });
+
+  resource.elementsOfKind('resources').forEach((sub) => {
+    output += renderResource(sub, level+1, uri);
+  });
+
+  return output;
+}
+
+
 function render(api, _options) {
   let output = '';
 
@@ -12,50 +55,12 @@ function render(api, _options) {
   output += '\n';
   output += 'type Query {\n';
   api.elementsOfKind('resources').forEach((resource) => {
-    processResource(resource);
+    output += renderResource(resource);
   });
   output += '}\n';
 
-  function processResource(resource, level = 0, parentUri = '') {
-    const rel = resource.attr('relativeUri').plainValue();
-    const uri = parentUri + rel;
-    // We will come back to pick up the types later
-
-    const methods = resource.elementsOfKind('methods');
-    methods.forEach((method) => {
-      // At this stage, we are only interested in GET, not mutations
-      if (method.name() === 'get') {
-        const args = [];
-
-        let basePath;
-        if (rel.startsWith('/{')) {
-          args.push(rel.replace(/\/{(.*)}/, '$1'));
-          basePath = parentUri;
-        } else {
-          basePath = uri;
-        }
-
-        const queryName = basePath.substr(1).replace('/', '-');
-        output += '  '.repeat(level) + queryName;
-        if (args.length > 0) {
-          output += `(${args.join(', ')})`;
-        }
-
-        const dna = resource.attr('displayName');
-        if (dna) {
-          output += ` # ${dna.plainValue()}`;
-        }
-
-        output += '\n';
-      }
-    });
-
-    resource.elementsOfKind('resources').forEach((sub) => {
-      processResource(sub, level+1, uri);
-    });
-  }
-
   return output;
 }
+
 
 exports.render = render;
