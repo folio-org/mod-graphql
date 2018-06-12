a78d27011ed4f9461dc579efbb323db882252daf
Mike Taylor <mike@indexdata.com>
2018-06-12 10:52:37 +0100

    Better naming for asSchema export

--- src/autogen/convertAPI.js
+++ src/autogen/convertAPI.js
@@ -1,6 +1,6 @@
 const raml = require('raml-1-parser');
 const { gatherAPI } = require('./gatherAPI');
-const asSchema = require('./asSchema');
+const { asSchema } = require('./asSchema');
 const { asResolvers } = require('./asResolvers');
 
 
@@ -29,7 +29,7 @@ function convertAPI(ramlName, resolveFunction, options) {
   }
 
   return {
-    schema: asSchema.render(gathered, options),
+    schema: asSchema(gathered, options),
     resolvers: resolveFunction ? asResolvers(gathered, resolveFunction, options) : null,
   };
 }
