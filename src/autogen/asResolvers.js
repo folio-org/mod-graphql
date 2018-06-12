888352d14d5e9d5498369f41c1d8272a9651f0e0
Mike Taylor <mike@indexdata.com>
2018-06-12 15:49:39 +0100

    Lint

--- src/autogen/asResolvers.js
+++ src/autogen/asResolvers.js
@@ -1,18 +1,18 @@
-function makeQueryResolvers(api, resolve, options) {
+function makeQueryResolvers(api, resolve, _options) {
   const resolvers = {};
 
   api.resources.forEach((r) => {
     const caption = r.displayName || r.queryName;
     const path = r.url.replace('http://localhost/', ''); // This doesn't feel quite right
-    resolvers[r.queryName] = (o, a, c) => resolve(o, a, c, caption, path)
+    resolvers[r.queryName] = (o, a, c) => resolve(o, a, c, caption, path);
     // We presently ignore the arguments: the resolve function knows about query, offset, etc.
   });
   return resolvers;
 }
 
-function asResolvers(api, resolve, options) {
+function asResolvers(api, resolve, _options) {
   return {
-    Query: makeQueryResolvers(api, resolve, options),
+    Query: makeQueryResolvers(api, resolve, _options),
     // Mutation: makeMutationResolvers(api, resolve, options),
   };
 }
