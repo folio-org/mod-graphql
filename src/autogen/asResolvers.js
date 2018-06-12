43fc2faf74da4635ce8de415957e623f03e461e3
Mike Taylor <mike@indexdata.com>
2018-06-12 13:07:16 +0100

    Provide simple hardwired instance_storage_instances resolver

--- src/autogen/asResolvers.js
+++ src/autogen/asResolvers.js
@@ -1,5 +1,17 @@
-function asResolvers(api, resolveFunction, options) {
-  return {};
+function makeQueryResolvers(api, resolve, options) {
+  return {
+    instance_storage_instances: (o, a, c) => resolve(o, a, c, 'instances', 'instance-storage/instances', null, null, {
+      records: 'instances',
+      totalCount: 'totalRecords',
+    }),
+  };
+}
+
+function asResolvers(api, resolve, options) {
+  return {
+    Query: makeQueryResolvers(api, resolve, options),
+    // Mutation: makeMutationResolvers(api, resolve, options),
+  };
 }
 
 
