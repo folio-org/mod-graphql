afa7c18c0a0d7a9902fa2ee4230cecc830c52834
Mike Taylor <mike@indexdata.com>
2018-06-12 17:54:58 +0100

    In line with the version in the `auto` branch

--- src/autogen/asResolvers.js
+++ src/autogen/asResolvers.js
@@ -1,15 +1,18 @@
-function makeQueryResolvers(api, resolve, options) {
-  return {
-    instance_storage_instances: (o, a, c) => resolve(o, a, c, 'instances', 'instance-storage/instances', null, null, {
-      records: 'instances',
-      totalCount: 'totalRecords',
-    }),
-  };
+function makeQueryResolvers(api, resolve, _options) {
+  const resolvers = {};
+
+  api.resources.forEach((r) => {
+    const caption = r.displayName || r.queryName;
+    const path = r.url.replace('http://localhost/', ''); // This doesn't feel quite right
+    resolvers[r.queryName] = (o, a, c) => resolve(o, a, c, caption, path);
+    // We presently ignore the arguments: the resolve function knows about query, offset, etc.
+  });
+  return resolvers;
 }
 
-function asResolvers(api, resolve, options) {
+function asResolvers(api, resolve, _options) {
   return {
-    Query: makeQueryResolvers(api, resolve, options),
+    Query: makeQueryResolvers(api, resolve, _options),
     // Mutation: makeMutationResolvers(api, resolve, options),
   };
 }
