ea788bd10f1d877ca7d48793b58ffefb92d760a2
Mike Taylor <mike@indexdata.com>
2018-06-13 16:51:29 +0100

    New file, tests schema generation

---
+++ tests/01-schema-generation.js
@@ -0,0 +1,19 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import fs from 'fs';
+import { testSchema, Status } from '../src/autogen/test/testSchema';
+
+describe('RAMLs and JSON schemas can be translated into GraphQL schemas', () => {
+  const dir = './src/autogen/test';
+  const counts = { total: 0, passed: 0, exceptions: 0, failed: 0 };
+  const errors = [];
+
+  fs.readdirSync(`${dir}/input`).forEach(file => {
+    if (file.match(/\.raml$/)) {
+      it(file, () => {
+        const res = testSchema(dir, file, false, counts, errors);
+        expect(res).not.to.equal(Status.FAIL);
+      });
+    }
+  });
+});
