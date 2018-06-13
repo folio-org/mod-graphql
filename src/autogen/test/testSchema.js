ed2ea650b55496685dc862d45d6b7e0be77b1380
Mike Taylor <mike@indexdata.com>
2018-06-13 16:46:20 +0100

    More explicit API
    What was I thinking, having a function that could indicate one of
    three results by returning true, false or null?

--- src/autogen/test/testSchema.js
+++ src/autogen/test/testSchema.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 const { convertAPI } = require('../convertAPI');
 
+const Status = {
+  PASS: 1,
+  FAIL: 2,
+  EXCEPTION: 3,
+};
+
 // This has a slightly complex API, because it has to provide all the
 // necessary information to two different callers -- ./run-tests.js
 // and ../../../tests/01-schema-generation.js
 //
-// It returns null if an expected exception occurred, otherwise true
-// if the generated schema matches the expectation and false if
+// It returns EXCEPTION if an expected exception occurred, otherwise
+// PASS if the generated schema matches the expectation and FAIL if
 // not. It also increments two of the members of the `counts` object:
 // `total` and one of `passed`, `failed`, `exceptions`. When
 // appropriate, it also pushes an error report onto the end of the
 // `errors` array.
 
-exports.testSchema = (dir, file, regen, counts, errors) => {
+function testSchema(dir, file, regen, counts, errors) {
   let schema, hadException = false;
   try {
     ({ schema } = convertAPI(`${dir}/input/${file}`, null, {}));
@@ -26,19 +32,22 @@ exports.testSchema = (dir, file, regen, counts, errors) => {
 
   if (regen) {
     fs.writeFileSync(schemaFile, schema);
-    return true;
+    return Status.PASS;
   }
 
   const expected = fs.readFileSync(schemaFile, 'utf8');
   if (expected !== schema) {
     counts.failed++;
     errors.push([file, expected, schema]);
-    return false;
+    return Status.FAIL;
   } else if (hadException) {
     counts.exceptions++;
-    return null;
+    return Status.EXCEPTION;
   } else {
     counts.passed++;
-    return true;
+    return Status.PASS;
   }
 };
+
+exports.testSchema = testSchema;
+exports.Status = Status;
