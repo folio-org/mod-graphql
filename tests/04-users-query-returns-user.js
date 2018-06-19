f96237bf4f86d8c554708ea4ed23e33d6abad575
Mike Taylor <mike@indexdata.com>
2018-06-19 12:42:42 +0100

    Update to use simpler testing-helper API

--- tests/04-users-query-returns-user.js
+++ tests/04-users-query-returns-user.js
@@ -1,8 +1,7 @@
-import { chai, expect, describe, it, beforeEach, OKAPI_TENANT, OKAPI_TOKEN } from './testlib/helper';
+/* eslint-disable no-unused-expressions */
 
+import { describe, beforeEach, runQuery, it, expect, UUIDregex } from './testlib/helper';
 import app from '../src/app';
-
-const UUIDregex = /^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[1-5][a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$/;
 
 describe('query returns a user with an ID and username', () => {
   if (process.env.AUTO_RESOLVERS) {
@@ -14,24 +13,8 @@ describe('query returns a user with an ID and username', () => {
 
   describe('query for all users', () => {
     let response;
-    beforeEach(() => {
-      return chai.request(app)
-        .post('/graphql')
-        .set('X-Okapi-Url', 'http://localhost:9131') // Uses the faked yakbak server
-        .set('X-Okapi-Tenant', OKAPI_TENANT)
-        .set('X-Okapi-Token', OKAPI_TOKEN)
-        .send({
-          query: 'query { users { id username } }',
-          variables: { limit: 3, query: 'diku_admin' },
-        })
-        .then(res => {
-          response = res;
-        })
-        .catch(err => {
-          console.error(`${err}`, JSON.parse(err.response.text));
-          throw err;
-        });
-    });
+    beforeEach(() => runQuery(app, 'query { users { id username } }')
+      .then(res => { response = res; }));
 
     it('contains a payload with users that have IDs', () => {
       expect(response, 'server returns a good response').to.have.status(200);
@@ -44,10 +27,8 @@ describe('query returns a user with an ID and username', () => {
       expect(record, 'records should be objects').to.be.instanceOf(Object);
       expect(Object.keys(record).length, 'exactly two fields should be included').to.equal(2);
       // See https://github.com/chaijs/chai/issues/56 for explanation of lint-disable
-      // eslint-disable-next-line no-unused-expressions
       expect(record.id, 'fields should include an ID').to.exist;
       expect(record.id, 'ID field should be a v4 UUID').to.match(UUIDregex);
-      // eslint-disable-next-line no-unused-expressions
       expect(record.username, 'fields should include a username').to.exist;
     });
   });
