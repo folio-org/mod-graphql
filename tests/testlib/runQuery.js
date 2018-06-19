9cce06c139970d03e6c7880149c012524abe4f8b
Mike Taylor <mike@indexdata.com>
2018-06-19 15:26:11 +0100

    Rename parameter

--- tests/testlib/runQuery.js
+++ tests/testlib/runQuery.js
@@ -1,8 +1,8 @@
 import { OKAPI_TENANT, OKAPI_TOKEN } from './env';
 import { request } from 'chai';
 
-export const runQuery = (xapp, query) => {
-  return request(xapp)
+export const runQuery = (app, query) => {
+  return request(app)
     .post('/graphql')
     .set('X-Okapi-Url', 'http://localhost:9131') // Uses the faked yakbak server
     .set('X-Okapi-Tenant', OKAPI_TENANT)
