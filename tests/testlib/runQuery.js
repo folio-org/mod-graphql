609f7c0318b73f09c0f031eb7bb579cf972b55ea
Mike Taylor <mike@indexdata.com>
2018-06-19 16:59:44 +0100

    runQuery supports an optional CQL-query argument

--- tests/testlib/runQuery.js
+++ tests/testlib/runQuery.js
@@ -1,13 +1,22 @@
 import { request } from 'chai';
 import { OKAPI_TENANT, OKAPI_TOKEN } from './env';
 
-function runQuery(app, query) {
+function runQuery(app, graphQLQuery, cqlQuery) {
+  const params = {
+    query: graphQLQuery
+  };
+  if (cqlQuery) {
+    params.variables = {
+      cql: cqlQuery
+    };
+  }
+
   return request(app)
     .post('/graphql')
     .set('X-Okapi-Url', 'http://localhost:9131') // Uses the faked yakbak server
     .set('X-Okapi-Tenant', OKAPI_TENANT)
     .set('X-Okapi-Token', OKAPI_TOKEN)
-    .send({ query })
+    .send(params)
     .catch(err => {
       console.error(`${err}`, JSON.parse(err.response.text));
       throw err;
