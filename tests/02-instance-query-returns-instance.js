7ae63043109bdc2c1859297b32bc2942311b1e02
Mike Taylor <mike@indexdata.com>
2018-06-19 12:33:42 +0100

    Move runQuery utility down into testlib

--- tests/02-instance-query-returns-instance.js
+++ tests/02-instance-query-returns-instance.js
@@ -1,19 +1,6 @@
-import { chai, expect, describe, it, beforeEach, OKAPI_TENANT, OKAPI_TOKEN, UUIDregex } from './testlib/helper';
+import { expect, describe, it, beforeEach, runQuery, UUIDregex } from './testlib/helper';
 
 import app from '../src/app';
-
-function runQuery(xapp, query) {
-  return chai.request(xapp)
-    .post('/graphql')
-    .set('X-Okapi-Url', 'http://localhost:9131') // Uses the faked yakbak server
-    .set('X-Okapi-Tenant', OKAPI_TENANT)
-    .set('X-Okapi-Token', OKAPI_TOKEN)
-    .send({ query })
-    .catch(err => {
-      console.error(`${err}`, JSON.parse(err.response.text));
-      throw err;
-    });
-}
 
 describe('query returns an instance with an ID and username', () => {
   describe('query for all instances', () => {
--- tests/testlib/helper.js
+++ tests/testlib/helper.js
@@ -1,11 +1,26 @@
 import mocha from 'mocha';
+import { request } from 'chai';
 import './yakbak';
 import './chai';
 
-export * from './env';
+import { OKAPI_TENANT, OKAPI_TOKEN } from './env';
+// export * from './env';
 
 export const { before, after, beforeEach, afterEach, describe, it } = mocha;
 
 export { default as chai, expect } from 'chai';
 
 export const UUIDregex = /^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[1-5][a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$/;
+
+export const runQuery = (xapp, query) => {
+  return request(xapp)
+    .post('/graphql')
+    .set('X-Okapi-Url', 'http://localhost:9131') // Uses the faked yakbak server
+    .set('X-Okapi-Tenant', OKAPI_TENANT)
+    .set('X-Okapi-Token', OKAPI_TOKEN)
+    .send({ query })
+    .catch(err => {
+      console.error(`${err}`, JSON.parse(err.response.text));
+      throw err;
+    });
+};
