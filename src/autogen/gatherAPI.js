ec6be6c566c19804432cc2a01e67a2dbc1bf8634
Mike Taylor <mike@indexdata.com>
2018-07-26 14:50:58 +0100

    More explicit diagnostic when no schema found
    Relevant to MODGQL-41.

--- src/autogen/gatherAPI.js
+++ src/autogen/gatherAPI.js
@@ -228,7 +228,7 @@ function gatherResource(resource, basePath, types, options, level = 0, parentUri
 
     const { schemaName, schemaText } = findResponseSchema(resource);
     if (!schemaName) {
-      throw new Error(`no schema for '${result.queryName}' in ${JSON.stringify(resource, null, 2)}`);
+      throw new Error(`no schema for '${result.queryName}': cannot find get/responses/200/body/schema or get/body/schema in ${JSON.stringify(resource, null, 2)}`);
     } else {
       // We shouldn't have to do this, but for some idiot reason when
       // raml.loadSync is unable to respolve a schema, it just sets
