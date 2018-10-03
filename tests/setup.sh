249ce6827df81ce4f9f5c34bef2f48bc90ce881f
Mike Taylor <mike@indexdata.com>
2018-10-03 11:21:08 +0100

    Nail down specific SHA1s for tested RAMLs
    This requires known versions not only of mod-inventory-storage but
    also the included raml-utils.
    
    At some point, we will want to upgrade mod-graphql so it can correctly
    deal with the way these modules use RAML 1.0 (which is not the same as
    the way as how the eHoldings module's RAML 1.0 worked). But that's
    another problem for a different day.
    
    Meanwhile, this should fix MODGQL-69.

--- tests/setup.sh
+++ tests/setup.sh
@@ -8,14 +8,24 @@ fi
 
 echo "Setting up tests ..."
 set -ex
+
 mkdir $DIR
 cd $DIR
 git clone --recurse-submodules https://github.com/folio-org/mod-inventory-storage
 # Stupidly, git clone yields zero exit-status even if the clone fails
 test -d mod-inventory-storage || exit 1
+
 cd mod-inventory-storage
-git checkout graphql
-# Stupidly, git clone yields zero exit-status even if the branch-switch fails
-git branch | grep '^\* graphql$' > /dev/null || exit 1
+BRANCH=d49b87c
+git checkout $BRANCH
+# Stupidly, git checkout yields zero exit-status even if the branch-switch fails
+git branch | grep '^\* .*'"$BRANCH"')*$' > /dev/null || exit 1
+
+cd ramls/raml-util
+BRANCH=df77247
+git checkout $BRANCH
+# Stupidly, git checkout yields zero exit-status even if the branch-switch fails
+git branch | grep '^\* .*'"$BRANCH"')*$' > /dev/null || exit 1
+
 { set +x; } 2>/dev/null
 echo "... all done"
