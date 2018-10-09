2507b9904b5e57497a0fbed64e5a3b172fa585b3
Mike Taylor <mike@indexdata.com>
2018-10-09 12:25:10 +0100

    Major change to top-level API
    app.js now exports a modGraphql function, which starts the service
    when invoked; previously, app.js would directly start the service and
    return a handle on the running app.
    
    The main purpose of this change is so that parameters can be passed
    into the modGraphql invocation -- specifically, the paths to the RAML
    files which are to be converted.
    
    Fixes MODGQL-79.

--- src/app.js
+++ src/app.js
@@ -7,61 +7,61 @@ import resolve from './resolve';
 import legacyResolvers from './resolvers';
 import { convertAPIs } from './autogen/convertAPI';
 
-let typeDefs;
-let resolvers;
-let logger;
-if (process.env.LEGACY_RESOLVERS) {
-  typeDefs = fs.readFileSync('./src/master.graphql', 'utf-8');
-  resolvers = legacyResolvers;
-} else {
-  let ramlPaths = ['tests/input/mod-inventory-storage/ramls/instance-storage.raml'];
-  // We need to avoid taking '--exit' as a RAML path, as `yarn test` specifies that
-  if (process.argv.length > 2 && process.argv[2] !== '--exit') {
-    ramlPaths = process.argv;
-    ramlPaths.shift();
-    ramlPaths.shift();
-  }
-  console.info(`using RAMLs ${ramlPaths.map(s => `'${s}'`).join(', ')}`);
-  ({ schema: typeDefs, resolvers, logger } = convertAPIs(ramlPaths, resolve));
-  logger.log('schema', `generated GraphQL schema:\n===\n${typeDefs}\n===`);
-}
-
-function badRequest(response, reason) {
-  response
-    .status(400)
-    .send(reason);
-}
-
-function checkOkapiHeaders(request, response, next) {
-  // console.info('GraphQL query:', JSON.stringify(request.body, null, 2));
-  if (!request.get('X-Okapi-Url')) {
-    badRequest(response, 'Missing Header: X-Okapi-Url');
-  } else if (!request.get('X-Okapi-Tenant')) {
-    badRequest(response, 'Missing Header: X-Okapi-Tenant');
-  } else if (!request.get('X-Okapi-Token')) {
-    badRequest(response, 'Missing Header: X-Okapi-Token');
+function modGraphql(ramlPaths) {
+  let typeDefs;
+  let resolvers;
+  let logger;
+  if (process.env.LEGACY_RESOLVERS) {
+    typeDefs = fs.readFileSync('./src/master.graphql', 'utf-8');
+    resolvers = legacyResolvers;
   } else {
-    next();
+    if (!ramlPaths) {
+      ramlPaths = ['tests/input/mod-inventory-storage/ramls/instance-storage.raml'];
+    }
+    console.info(`using RAMLs ${ramlPaths.map(s => `'${s}'`).join(', ')}`);
+    ({ schema: typeDefs, resolvers, logger } = convertAPIs(ramlPaths, resolve));
+    logger.log('schema', `generated GraphQL schema:\n===\n${typeDefs}\n===`);
   }
+
+  function badRequest(response, reason) {
+    response
+      .status(400)
+      .send(reason);
+  }
+
+  function checkOkapiHeaders(request, response, next) {
+    // console.info('GraphQL query:', JSON.stringify(request.body, null, 2));
+    if (!request.get('X-Okapi-Url')) {
+      badRequest(response, 'Missing Header: X-Okapi-Url');
+    } else if (!request.get('X-Okapi-Tenant')) {
+      badRequest(response, 'Missing Header: X-Okapi-Tenant');
+    } else if (!request.get('X-Okapi-Token')) {
+      badRequest(response, 'Missing Header: X-Okapi-Token');
+    } else {
+      next();
+    }
+  }
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  return express()
+    .post('/graphql', bodyParser.json(), checkOkapiHeaders, graphqlExpress(request => ({
+      schema,
+      // debug: false, // if you don't want error objects passed to console.error()
+      context: {
+        query: request.body,
+        okapi: {
+          url: process.env.OKAPI_URL || request.get('X-Okapi-Url'),
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-Okapi-Tenant': request.get('X-Okapi-Tenant'),
+            'X-Okapi-Token': request.get('X-Okapi-Token')
+          }
+        },
+        logger,
+      }
+    })));
 }
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-export default express()
-  .post('/graphql', bodyParser.json(), checkOkapiHeaders, graphqlExpress(request => ({
-    schema,
-    // debug: false, // if you don't want error objects passed to console.error()
-    context: {
-      query: request.body,
-      okapi: {
-        url: process.env.OKAPI_URL || request.get('X-Okapi-Url'),
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'X-Okapi-Tenant': request.get('X-Okapi-Tenant'),
-          'X-Okapi-Token': request.get('X-Okapi-Token')
-        }
-      },
-      logger,
-    }
-  })));
+export default modGraphql;
