03cdd5b47b4a78898fec6c813c9e53f48977146b
Mike Taylor <mike@indexdata.com>
2018-10-12 11:54:27 +0100

    Rip out all query resolvers
    We actually don't need this source file at all as code, but I want to
    keep it around for now so I can consult the old source when I come to
    implement mutations.
    
    Part of MODGQL-81.

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,45 +1,10 @@
 import fetch from 'node-fetch';
 import _ from 'lodash';
-import resolve from './resolve';
 
+// We'll keep the file around for now so we can consult its mutation support
+console.error('this code is no longer supported');
 
 const resolvers = {
-  Query: {
-    hello: () => 'hi!',
-    users: (o, a, c) => resolve(o, a, c, 'users', 'users', null, null, 'users'),
-    groups: (o, a, c) => resolve(o, a, c, 'groups', 'groups', null, null, 'usergroups'),
-    instance_storage_instances: (o, a, c) => resolve(o, a, c, 'instances', 'instance-storage/instances'),
-    instance_storage_instances_SINGLE: (o, a, c) => resolve(o, a, c, 'instance', 'instance-storage/instances/{instanceId}'),
-  },
-
-  Metadata: {
-    createdByUser: (o, a, c) => resolve(o, a, c, 'createdByUser', 'users/[createdByUserId]'),
-    updatedByUser: (o, a, c) => resolve(o, a, c, 'updatedByUser', 'users/[updatedByUserId]'),
-  },
-
-  Instance: {
-    instanceType: (o, a, c) => resolve(o, a, c, 'instanceType', 'instance-types/[instanceTypeId]'),
-    instanceFormat: (o, a, c) => resolve(o, a, c, 'instanceFormat', 'instance-formats/[instanceFormatId]'),
-    holdingsRecords: (o, a, c) => resolve(o, a, c, 'holdings', 'holdings-storage/holdings', 'id', 'instanceId', 'holdingsRecords'),
-  },
-
-  Identifier: {
-    identifierType: (o, a, c) => resolve(o, a, c, 'identifierType', 'identifier-types/[identifierTypeId]'),
-  },
-
-  Contributor: {
-    contributorType: (o, a, c) => resolve(o, a, c, 'contributorType', 'contributor-types/[contributorTypeId]'),
-    contributorNameType: (o, a, c) => resolve(o, a, c, 'contributorNameType', 'contributor-name-types/[contributorNameTypeId]'),
-  },
-
-  Classification: {
-    classificationType: (o, a, c) => resolve(o, a, c, 'classificationType', 'classification-types/[classificationTypeId]'),
-  },
-
-  HoldingsRecord: {
-    holdingsItems: (o, a, c) => resolve(o, a, c, 'items', 'inventory/items', 'id', 'holdingsRecordId', 'items'),
-  },
-
   Mutation: {
     updateUser: (root, updated, { okapi }) => {
       // We don't currently support PATCH so we'll need to grab the record to base our update on
