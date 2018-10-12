98ec29c84df8364016200d1bab05b64d3630d184
Mike Taylor <mike@indexdata.com>
2018-10-12 15:25:06 +0100

    Rename variable

--- tests/04-schema-generation.js
+++ tests/04-schema-generation.js
@@ -5,20 +5,20 @@ import { testSchema, Status } from '../src/autogen/test/testSchema';
 
 describe('04. RAMLs and JSON schemas can be translated into GraphQL schemas', () => {
   const dir = './src/autogen/test';
-  const counts = { total: 0, passed: 0, exceptions: 0, failed: 0 };
+  const c = { total: 0, passed: 0, exceptions: 0, failed: 0 };
   const errors = [];
 
   fs.readdirSync(`${dir}/input`).forEach(file => {
     if (file.match(/\.raml$/)) {
       it(file, () => {
-        const res = testSchema(dir, file, false, counts, errors);
+        const res = testSchema(dir, file, false, c, errors);
         expect(res).not.to.equal(Status.FAIL);
       });
     }
   });
 
   after(() => {
-    console.info(`      : ${counts.total} tests: ${counts.passed} passed, ${counts.exceptions} expected exceptions, ${counts.failed} failed`);
+    console.info(`      : ${c.total} tests: ${c.passed} passed, ${c.exceptions} expected exceptions, ${c.failed} failed`);
     if (errors.length) {
       errors.forEach((e) => {
         const [name, expected, got] = e;
