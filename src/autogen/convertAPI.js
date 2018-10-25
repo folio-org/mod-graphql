055163117a44975880302857e844d6ea6f1ea133
Mike Taylor <mike@indexdata.com>
2018-10-25 13:08:56 +0100

    require mergeAPIs from its own new source-file
    Part of MODGQL-116.

--- src/autogen/convertAPI.js
+++ src/autogen/convertAPI.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const raml = require('raml-1-parser');
-const { isEqual, findIndex, some } = require('lodash');
+const { findIndex, some } = require('lodash');
 const { gatherAPI } = require('./gatherAPI');
+const { mergeAPIs } = require('./mergeAPIs');
 const { asSchema } = require('./asSchema');
 const { asResolvers } = require('./asResolvers');
 
@@ -98,96 +99,6 @@ function parseAndGather(ramlName, resolveFunction, options) {
 }
 
 
-// Each APIs comments are represented by an ordered list. Each
-// list-member is itself a list of two elements: the first is the
-// comment-name (version, protocols, baseUrl, etc.) and the second is
-// an ordered list of values.
-//
-// The merged list retains the original order as far as possible: the
-// comments that exist in the first entry, in their original order;
-// followed by those comments that exist in the second but not the
-// first, in order; and so on.
-//
-function mergeComments(list) {
-  const register = {};
-  const res = [];
-
-  list.forEach(comments => {
-    comments.forEach(comment => {
-      const [name, values] = comment;
-      if (!register[name]) {
-        register[name] = [name, values];
-        res.push(register[name]);
-      } else {
-        register[name][1].push(...values);
-      }
-    });
-  });
-
-  return res;
-}
-
-
-// For this one, it's just a matter of concatenating the array and
-// complaining if there's a duplicate queryName.
-//
-function mergeResources(list) {
-  const register = {};
-  const res = [];
-
-  list.forEach(tmp => {
-    const [ramlName, resources] = tmp;
-    resources.forEach(resource => {
-      const name = resource.queryName;
-      if (!register[name]) {
-        register[name] = ramlName;
-        res.push(resource);
-      } else {
-        throw Error(`duplicate resource name '${name}' in ${register[name]} and ${ramlName}`);
-      }
-    });
-  });
-
-  return res;
-}
-
-
-// Similar to mergeResources, but each API's types are a map rather
-// than a list, and duplicate definitions are OK provided they're
-// identical.
-//
-function mergeTypes(list, options) {
-  const res = {};
-
-  list.forEach(types => {
-    Object.keys(types).forEach(name => {
-      const type = types[name];
-      if (!res[name]) {
-        res[name] = type;
-      } else {
-        const same = isEqual(type, res[name]);
-        if (same) {
-          options.logger.log('duptype', `duplicate type name '${name}' with same definition`);
-        } else {
-          throw Error(`duplicate type name '${name}' with different definition`);
-        }
-      }
-    });
-  });
-
-  return res;
-}
-
-
-function mergeAPIs(list, options) {
-  return {
-    comments: mergeComments(list.map(api => api.comments)),
-    resources: mergeResources(list.map(api => [api.ramlName, api.resources])),
-    types: mergeTypes(list.map(api => api.types), options),
-  };
-}
-
-
 // Reads the RAML files whose names are specified in the `ramlNames`
 // array, together with whatever traits, other fragments and JSON
 // Schemas they use. Converts the result into a GraphQL schema and a
@@ -217,6 +128,3 @@ function convertAPIs(ramlNames, resolveFunction, baseOptions) {
 
 
 exports.convertAPIs = convertAPIs;
-exports._TESTING_mergeComments = mergeComments;
-exports._TESTING_mergeResources = mergeResources;
-exports._TESTING_mergeTypes = mergeTypes;
