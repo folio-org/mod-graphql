10a3c86114f744c00e28ae8ef207514c5f871f2f
Mike Taylor <mike@indexdata.com>
2018-10-25 13:08:32 +0100

    New file mergeAPIs
    Code removed from convertAPI.
    
    Fixes MODGQL-116.

---
+++ src/autogen/mergeAPIs.js
@@ -0,0 +1,96 @@
+const { isEqual } = require('lodash');
+
+// Each APIs comments are represented by an ordered list. Each
+// list-member is itself a list of two elements: the first is the
+// comment-name (version, protocols, baseUrl, etc.) and the second is
+// an ordered list of values.
+//
+// The merged list retains the original order as far as possible: the
+// comments that exist in the first entry, in their original order;
+// followed by those comments that exist in the second but not the
+// first, in order; and so on.
+//
+function mergeComments(list) {
+  const register = {};
+  const res = [];
+
+  list.forEach(comments => {
+    comments.forEach(comment => {
+      const [name, values] = comment;
+      if (!register[name]) {
+        register[name] = [name, values];
+        res.push(register[name]);
+      } else {
+        register[name][1].push(...values);
+      }
+    });
+  });
+
+  return res;
+}
+
+
+// For this one, it's just a matter of concatenating the array and
+// complaining if there's a duplicate queryName.
+//
+function mergeResources(list) {
+  const register = {};
+  const res = [];
+
+  list.forEach(tmp => {
+    const [ramlName, resources] = tmp;
+    resources.forEach(resource => {
+      const name = resource.queryName;
+      if (!register[name]) {
+        register[name] = ramlName;
+        res.push(resource);
+      } else {
+        throw Error(`duplicate resource name '${name}' in ${register[name]} and ${ramlName}`);
+      }
+    });
+  });
+
+  return res;
+}
+
+
+// Similar to mergeResources, but each API's types are a map rather
+// than a list, and duplicate definitions are OK provided they're
+// identical.
+//
+function mergeTypes(list, options) {
+  const res = {};
+
+  list.forEach(types => {
+    Object.keys(types).forEach(name => {
+      const type = types[name];
+      if (!res[name]) {
+        res[name] = type;
+      } else {
+        const same = isEqual(type, res[name]);
+        if (same) {
+          options.logger.log('duptype', `duplicate type name '${name}' with same definition`);
+        } else {
+          throw Error(`duplicate type name '${name}' with different definition`);
+        }
+      }
+    });
+  });
+
+  return res;
+}
+
+
+function mergeAPIs(list, options) {
+  return {
+    comments: mergeComments(list.map(api => api.comments)),
+    resources: mergeResources(list.map(api => [api.ramlName, api.resources])),
+    types: mergeTypes(list.map(api => api.types), options),
+  };
+}
+
+
+exports.mergeAPIs = mergeAPIs;
+exports._TESTING_mergeComments = mergeComments;
+exports._TESTING_mergeResources = mergeResources;
+exports._TESTING_mergeTypes = mergeTypes;
