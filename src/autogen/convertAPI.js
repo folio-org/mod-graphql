19a176fc961c13547b2db43de890fc72c5743a5b
Mike Taylor <mike@indexdata.com>
2018-10-25 11:26:51 +0100

    Use 'endmap' instead of 'mapend'

--- src/autogen/convertAPI.js
+++ src/autogen/convertAPI.js
@@ -55,7 +55,7 @@ function parseSchemaMap(ramlName, options) {
       }
       const match = line.match(schemaRegexp);
       if (!match) {
-        options.logger.log('mapend', `unexpected end to schema map for ${ramlName}:`, line);
+        options.logger.log('endmap', `unexpected end to schema map for ${ramlName}:`, line);
         break;
       }
       const path = match[2];
