76512e434c7c2e5818f054bc815ea0a80a59ad01
Mike Taylor <mike@indexdata.com>
2019-01-09 23:49:32 +0000

    Move overlaySchema out to its own file

--- src/merge/merge-schemas.js
+++ src/merge/merge-schemas.js
@@ -7,26 +7,7 @@ function parseSchema(schemaName) {
 }
 
 const baseSchema = parseSchema('../../tests/mod-inventory-storage-ramls/instance.json');
+const overlaySchema = parseSchema('overlaySchema.json');
 
-const overlaySchema = {
-  type: 'object',
-  properties: {
-    holdingsRecords3: {
-      type: "array",
-      description: "List of holdings records",
-      items: {
-        type: "object",
-        $ref: "holdingsrecord.json"
-      },
-      readonly: true,
-      "folio:isVirtual": true,
-      "folio:linkBase": "holdings-storage/holdings",
-      "folio:linkFromField": "id",
-      "folio:linkToField": "instanceId",
-      "folio:includedElement": "holdingsRecords"
-    },
-  },
-};
-
-const m1 = mergeJsonSchemas([baseSchema, overlaySchema]);
-console.log(JSON.stringify(m1, null, 2));
+const merged = mergeJsonSchemas([baseSchema, overlaySchema]);
+console.log(JSON.stringify(merged, null, 2));
