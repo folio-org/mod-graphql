64e0b36721bb87c0a7f77960257b08a72852d013
Mike Taylor <mike@indexdata.com>
2023-02-21 17:28:33 +0000

    Added new test of ability to merge all productions schemas
    At this point, I am hoping to see the PR rejected due to test failure.

---
+++ tests/20-merge-production-schemas.js
@@ -0,0 +1,30 @@
+/* eslint-disable prefer-arrow-callback, func-names */
+
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+
+const execSync = require('child_process').execSync;
+
+describe('01. merge production schemas', function () {
+  it('compile and merge', function (done) {
+    this.timeout(10000);
+
+    // XXX this must run against the same schemas listed on the CMD line of ../Dockerfile
+    const command = './src/autogen/raml2graphql ./build/schemas-for-build/*/ramls/*.raml';
+    let output;
+    try {
+      output = execSync(command);
+    } catch (e) {
+      console.error('failed:', typeof e, e.toString());
+      done(e);
+      return;
+    }
+
+    // The process suceeded; check that the output is expected RAML
+    const s = output.toString();
+    assert(s.match(/^# title: /m), 'output contains title');
+    assert(s.match(/^# version: /m), 'output contains version');
+    assert(s.match(/^type Query {$/m), 'output contains type Query');
+    done();
+  });
+});
