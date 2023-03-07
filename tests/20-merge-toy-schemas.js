c0eb278ab06420ee8dcffe42bbcd468ac57f3479
Mike Taylor <mike@miketaylor.org.uk>
2023-03-07 12:10:02 +0000

    Fix some ESLint errors (mostly just whitespace) (#99)
    Fixes MODGQL-171.

--- tests/20-merge-toy-schemas.js
+++ tests/20-merge-toy-schemas.js
@@ -7,51 +7,51 @@ const testCases = [
     name: 'different schema name, matching definition',
     services: ['service1', 'service2'],
     matches: [
-      [ true, /^# title: First/m ],
-      [ true, /^# title: .*Second/m ],
-      [ true, /^type Tws1_person_SINGLE__person /m ],
-      [ true, /^type Tws2_person_SINGLE__basicPerson /m ],
+      [true, /^# title: First/m],
+      [true, /^# title: .*Second/m],
+      [true, /^type Tws1_person_SINGLE__person /m],
+      [true, /^type Tws2_person_SINGLE__basicPerson /m],
     ],
   },
   {
     name: 'same schema name, matching definition',
     services: ['service1', 'service3'],
     matches: [
-      [ true, /^# title: First/m ],
-      [ true, /^# title: .*Third/m ],
-      [ true, /^type Tws1_person_SINGLE__person /m ],
-      [ false, /type Tws3_person_SINGLE__basicPerson/ ],
+      [true, /^# title: First/m],
+      [true, /^# title: .*Third/m],
+      [true, /^type Tws1_person_SINGLE__person /m],
+      [false, /type Tws3_person_SINGLE__basicPerson/],
     ],
   },
   {
     name: 'different schema name, different definition',
     services: ['service1', 'service4'],
     matches: [
-      [ true, /^# title: First/m ],
-      [ true, /^# title: .*Fourth/m ],
-      [ true, /^type Tws1_person_SINGLE__person /m ],
-      [ true, /^type Tws4_person_SINGLE__advancedPerson /m ],
+      [true, /^# title: First/m],
+      [true, /^# title: .*Fourth/m],
+      [true, /^type Tws1_person_SINGLE__person /m],
+      [true, /^type Tws4_person_SINGLE__advancedPerson /m],
     ],
   },
   {
     name: 'same schema name, different definition',
     services: ['service1', 'service5'],
     matches: [
-      [ true, /^# title: First/m ],
-      [ true, /^# title: .*Fifth/m ],
-      [ true, /^type Tws1_person_SINGLE__person /m ],
-      [ true, /^type Tws5_person_SINGLE__person /m ],
+      [true, /^# title: First/m],
+      [true, /^# title: .*Fifth/m],
+      [true, /^type Tws1_person_SINGLE__person /m],
+      [true, /^type Tws5_person_SINGLE__person /m],
     ],
   },
   {
     name: 'schema included at multiple levels by different resources',
     services: ['service1', 'service6'],
     matches: [
-      [ true, /^# title: First/m ],
-      [ true, /^# title: .*Sixth/m ],
-      [ true, /^type T_basic_person /m ],
-      [ true, /^type Tws1_person_SINGLE__person /m ],
-      [ true, /^type Tws6_person_SINGLE__person /m ],
+      [true, /^# title: First/m],
+      [true, /^# title: .*Sixth/m],
+      [true, /^type T_basic_person /m],
+      [true, /^type Tws1_person_SINGLE__person /m],
+      [true, /^type Tws6_person_SINGLE__person /m],
     ],
   },
 ];
@@ -76,8 +76,8 @@ describe('20. merge toy schemas', () => {
       // console.log(s);
       matches.forEach(matchCase => {
         const [shouldMatch, regexp] = matchCase;
-        const matches = s.match(regexp);
-        assert(!!matches === shouldMatch, (shouldMatch ? 'does not match' : 'matches') + ' ' + regexp);
+        const doesMatch = s.match(regexp);
+        assert(!!doesMatch === shouldMatch, (shouldMatch ? 'does not match' : 'matches') + ' ' + regexp);
       });
       done();
     });
