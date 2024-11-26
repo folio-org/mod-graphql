543e17125c34ed37dffa339ff09b180e0578fa2e
Mike Taylor <mike@indexdata.com>
2024-11-26 21:29:39 +0000

    Reorder lines to better match old version

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import Logger from './src/configuredLogger.js';
 import modGraphql from './src/app.js';
 
+const logger = new Logger();
+const httpServer = await modGraphql(logger, process.argv.slice(2));
+
 if (process.env.CONSOLE_TRACE) {
   // The code by Remy Sharp
   // https://remysharp.com/2014/05/23/where-is-that-console-log/
@@ -19,8 +22,6 @@ if (process.env.CONSOLE_TRACE) {
   });
 }
 
-const logger = new Logger();
-const httpServer = await modGraphql(logger, process.argv.slice(2));
 const port = 3001; // XXX should provide a way to change this
 await new Promise(resolve2 => httpServer.listen({ port }, resolve2));
 logger.log('listen', `listening on port ${port}`);
