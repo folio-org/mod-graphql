e9a42df8a5cb4de8f9d10d5a79bf93819c4a3fd9
Mike Taylor <mike@indexdata.com>
2024-11-26 18:55:04 +0000

    Limping along

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 import Logger from './src/configuredLogger.js';
 import modGraphql from './src/app.js';
 
-const logger = new Logger();
-const app = modGraphql(process.argv.slice(2));
-
 if (process.env.CONSOLE_TRACE) {
   // The code by Remy Sharp
   // https://remysharp.com/2014/05/23/where-is-that-console-log/
@@ -22,6 +19,11 @@ if (process.env.CONSOLE_TRACE) {
   });
 }
 
+const logger = new Logger();
+const httpServer = modGraphql(process.argv.slice(2));
+/*
 const port = 3001; // XXX should provide a way to change this
 logger.log('listen', `listening on port ${port}`);
-app.listen(port);
+await new Promise(resolve => httpServer.listen({ port }, resolve));
+console.log('🚀 Server ready 2');
+*/
--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import http from 'http';
+import cors from 'cors';
 import Logger from './configuredLogger.js';
 import { listAPIs } from './autogen/listAPIs.js';
 import resolve from './resolve.js';
 import { convertAPIs } from './autogen/convertAPI.js';
 
-function modGraphql(argv) {
+async function modGraphql(argv) {
   const ramlPaths = argv[0] === '-a' ? listAPIs(argv[1]) : argv;
   if (!ramlPaths || ramlPaths.length === 0) throw Error('modGraphql invoked with no RAMLpaths');
   const logger = new Logger();
@@ -25,48 +29,60 @@ function modGraphql(argv) {
   function checkOkapiHeaders(request, response, next) {
     // console.info('GraphQL query:', JSON.stringify(request.body, null, 2));
     if (!request.get('X-Okapi-Url')) {
-      badRequest(response, 'Missing Header: X-Okapi-Url');
+      // XXX badRequest(response, 'Missing Header: X-Okapi-Url');
     } else if (!request.get('X-Okapi-Tenant')) {
-      badRequest(response, 'Missing Header: X-Okapi-Tenant');
+      // XXX badRequest(response, 'Missing Header: X-Okapi-Tenant');
     } else if (!request.get('X-Okapi-Token')) {
-      badRequest(response, 'Missing Header: X-Okapi-Token');
+      // XXX badRequest(response, 'Missing Header: X-Okapi-Token');
     } else {
       next();
     }
   }
 
+  const app = express();
+  const httpServer = http.createServer(app);
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req, res }) => {
-      // Trying to do this by hand, not sure if it will work
-      checkOkapiHeaders(req, res, () => undefined);
-
-      const { OKAPI_URL, OKAPI_TENANT, OKAPI_TOKEN } = process.env;
-
-      return {
-        query: req.body,
-        okapi: {
-          url: OKAPI_URL || req.get('X-Okapi-Url'),
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'X-Okapi-Tenant': OKAPI_TENANT || req.get('X-Okapi-Tenant'),
-            'X-Okapi-Token': OKAPI_TOKEN || req.get('X-Okapi-Token')
-          }
-        },
-        logger,
-      };
-    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
-  const app = express();
+  await server.start();
+  console.log('server started');
+
   app.get('/admin/health', (req, res) => {
     res.send('Behold! I live!!');
   });
 
-  server.applyMiddleware({ app, cors: false });
-  return app;
+  app.use('/',
+    cors(),
+    express.json({ limit: '50mb' }),
+    expressMiddleware(server, {
+      context: ({ req, res }) => {
+        // Trying to do this by hand, not sure if it will work
+        checkOkapiHeaders(req, res, () => undefined);
+
+        const { OKAPI_URL, OKAPI_TENANT, OKAPI_TOKEN } = process.env;
+
+        return {
+          query: req.body,
+          okapi: {
+            url: OKAPI_URL || req.get('X-Okapi-Url'),
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'X-Okapi-Tenant': OKAPI_TENANT || req.get('X-Okapi-Tenant'),
+              'X-Okapi-Token': OKAPI_TOKEN || req.get('X-Okapi-Token')
+            }
+          },
+          logger,
+        };
+      },
+    }));
+
+  await new Promise(resolve2 => httpServer.listen({ port: 3001 /* XXX */ }, resolve2));
+  console.log('🚀 Server ready 1');
+  return httpServer;
 }
 
 export default modGraphql;
